0bd0be8f89fe15561295482f75cd35c17e98cbc5
Léo DUMONT <leo.dumont@epitech.eu>
2024-12-05 11:18:39 +0100

    feat(view-each): added each() method to the view implementation.

--- lib/View.hpp
+++ lib/View.hpp
@@ -63,6 +63,63 @@ class View
 
 public:
     View(const std::map<std::size_t, ISparseSet *> &sparse_sets) : _type_ids{type<Component>::id(), type<Others>::id()...}, _sparse_sets{sparse_sets} {
+        _queryEntities();
+    }
+    ~View() = default;
+
+    void refresh() {
+        _queryEntities();
+    }
+
+    Iterator begin() {
+        return Iterator(_entities.begin(), _sparse_sets);
+    }
+
+    Iterator end() {
+        return Iterator(_entities.end(), _sparse_sets);
+    }
+
+    template <typename Func>
+    void each(Func&& func) {
+        for (auto entity : _entities) {
+            std::tuple<Component&, Others&...> tuple = std::tie(dynamic_cast<SparseSet<Component>&>(*_sparse_sets.at(type<Component>::id())).get(entity),
+                                     dynamic_cast<SparseSet<Others>&>(*_sparse_sets.at(type<Others>::id())).get(entity)...);
+            if constexpr (std::is_invocable_v<Func, Entity, Component&, Others&...>) {
+                func(entity, std::get<Component&>(tuple), std::get<Others&>(tuple)...);
+            } else {
+                func(std::get<Component&>(tuple), std::get<Others&>(tuple)...);
+            }
+        }
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const View &view)
+    {
+        std::vector<Entity> identifiers = view._type_ids;
+        if (identifiers.empty())
+            return os;
+        os << "Template ids:" << std::endl << identifiers[0];
+        identifiers.erase(identifiers.begin());
+        for (const auto id : identifiers)
+            os << ", " << id;
+        os << std::endl;
+        os << "Entities:" << std::endl;
+        auto entities = view._entities;
+        if (entities.empty())
+            return os;
+        os << entities[0];
+        entities.erase(entities.begin());
+        for (const auto entity : entities)
+            os << ", " << entity;
+        os << std::endl;
+        return os;
+    }
+
+private:
+    std::map<std::size_t, ISparseSet *> _sparse_sets;
+    std::vector<std::size_t> _type_ids;
+    std::vector<Entity> _entities;
+
+    void _queryEntities() {
         for (const auto &component : _type_ids)
         {
             if (!_sparse_sets.contains(component))
@@ -90,102 +147,6 @@ public:
 
         _entities = entitiesList;
     }
-    ~View() = default;
-
-    Iterator begin() {
-        return Iterator(_entities.begin(), _sparse_sets);
-    }
-
-    Iterator end() {
-        return Iterator(_entities.end(), _sparse_sets);
-    }
-
-    // std::vector<std::pair<Entity, std::tuple<Component, Others...>>> get()
-    // {
-    //     std::vector<std::tuple<Component, Others...>> components;
-    //     std::vector<Entity> entities;
-    //
-    //     for (const auto &component : _type_ids)
-    //     {
-    //         if (!_sparse_sets.contains(component))
-    //         {
-    //             std::cerr << "Component " << component << " is not in the registry" << std::endl;
-    //             return {};
-    //         }
-    //     }
-    //
-    //     const auto first = _type_ids[0];
-    //     const auto sparse = dynamic_cast<SparseSet<Component> *>(_sparse_sets.at(first));
-    //
-    //     auto entitiesList = sparse->getEntities();
-    //     for (const auto &entity : sparse->getEntities())
-    //     {
-    //         std::tuple<Component, Others...> tuple;
-    //         Component &res = sparse->get(entity);
-    //         std::get<Component>(tuple) = sparse->get(entity);
-    //         if constexpr (sizeof...(Others) > 0)
-    //         {
-    //             if (_updateComponentsTuple<Others...>(entity, tuple))
-    //             {
-    //                 components.push_back(tuple);
-    //                 entities.push_back(entity);
-    //             }
-    //         }
-    //         else
-    //         {
-    //             components.push_back(tuple);
-    //             entities.push_back(entity);
-    //         }
-    //     }
-    //
-    //     std::vector<std::pair<Entity, std::tuple<Component, Others...>>> result;
-    //     for (Entity i = 0; i < components.size(); i++)
-    //     {
-    //         result.push_back(std::make_pair(entities[i], components[i]));
-    //     }
-    //     return result;
-    // }
-
-    friend std::ostream &operator<<(std::ostream &os, const View &view)
-    {
-        std::vector<Entity> identifiers = view._type_ids;
-        if (identifiers.empty())
-            return os;
-        os << identifiers[0];
-        identifiers.erase(identifiers.begin());
-        for (const auto id : identifiers)
-            os << ", " << id;
-        return os;
-    }
-
-private:
-    std::map<std::size_t, ISparseSet *> _sparse_sets;
-    std::vector<std::size_t> _type_ids;
-    std::vector<Entity> _entities;
-
-    // template <typename T>
-    // bool _updateComponentTuple(Entity entity, std::tuple<Component, Others...> &tuple)
-    // {
-    //     const auto id = type<T>::id();
-    //     auto sparse = dynamic_cast<SparseSet<T> *>(_sparse_sets.at(id));
-    //     std::get<T>(tuple) = sparse->get(entity);
-    //     return true;
-    // }
-    //
-    // template <typename T, typename... Remaining>
-    // bool _updateComponentsTuple(Entity entity, std::tuple<Component, Others...> &tuple)
-    // {
-    //     const auto id = type<T>::id();
-    //     auto sparse = dynamic_cast<SparseSet<T> *>(_sparse_sets.at(id));
-    //     if (!sparse->contains(entity))
-    //         return false;
-    //     std::get<T>(tuple) = sparse->get(entity);
-    //     if constexpr (sizeof...(Remaining) > 0)
-    //     {
-    //         return _updateComponentTuple<Remaining...>(entity, tuple);
-    //     }
-    //     return true;
-    // }
 
     template <typename T, typename... Remaining>
     bool _entityContainComponents(const Entity entity)
