d9273baa30f823bb7ce0dfafa7fcda4caf813346
Léo DUMONT <leo.dumont@epitech.eu>
2024-12-16 00:28:36 +0100

    chore(server-network): updated server network and added draft code to show how packets work.

--- server/src/Server.hpp
+++ server/src/Server.hpp
@@ -12,9 +12,52 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <functional>
+#include <map>
 #include "NetworkAgent.hpp"
 
 #define TARGET_TICKRATE 30
+
+/**
+ * @brief This is temporary, for the example of how packets work.
+ */
+const std::map<PACKET_TYPE, std::function<void(UDPPacket& packet)>> packet_handlers = {
+    {
+        PACKET_TYPE::CONNECT, [](UDPPacket& packet)
+        {
+            std::cout << "Received CONNECT packet." << std::endl;
+            std::string connect_message;
+            packet >> connect_message;
+            std::cout << "Message: " << connect_message << std::endl;
+        }
+    },
+    {
+        PACKET_TYPE::DISCONNECT, [](UDPPacket& packet)
+        {
+            std::cout << "Received DISCONNECT packet." << std::endl;
+        }
+    },
+    {
+        PACKET_TYPE::MESSAGE, [](UDPPacket& packet)
+        {
+            std::cout << "Received MESSAGE packet." << std::endl;
+
+            std::string message;
+            packet >> message;
+            std::cout << "Message: " << message << std::endl;
+        }
+    },
+    {
+        PACKET_TYPE::POSITION, [](UDPPacket& packet)
+        {
+            std::cout << "Received POSITION packet." << std::endl;
+
+            Position pos{};
+            packet >> pos;
+            std::cout << "Position: (" << pos.x << ", " << pos.y << ")" << std::endl;
+        }
+    }
+};
 
 class Server: public NetworkAgent
 {
@@ -30,7 +73,7 @@ public:
      */
     void run()
     {
-        std::uint32_t target_tick_delta = 1000 / TARGET_TICKRATE;
+         constexpr std::uint32_t target_tick_delta = 1000 / TARGET_TICKRATE;
 
         this->_running = true;
         std::cout << "Game is running..." << std::endl;
@@ -49,11 +92,20 @@ public:
     }
 
 private:
-    virtual void _onPacketReceived(const asio::ip::udp::endpoint& src, const UDPPacket& packet)
+    void _onPacketReceived(const asio::ip::udp::endpoint& src, UDPPacket& packet) override
     {
-        std::string msg(packet.payload.begin(), packet.payload.end());
-        std::cout << "Received: " << msg << " (seq: " << packet.sequence_number
-                  << ", ack: " << packet.ack_number << ")" << std::endl;
+        const auto payload = packet.payload;
+
+        std::cout << "Received: " << payload << " (seq: " << packet.sequence_number
+            << ", ack: " << packet.ack_number << ")" << std::endl;
+
+        /**
+         * @brief This code is temporary!!!
+        */
+        PACKET_TYPE packet_type{};
+        packet >> packet_type;
+         if (packet_handlers.contains(packet_type))
+            packet_handlers.at(packet_type)(packet);
 
         auto it = std::find(this->_clients.begin(), this->_clients.end(), src);
         if (it == this->_clients.end())
@@ -62,8 +114,10 @@ private:
 
     void _broadcast(const std::string& message)
     {
+        UDPPacket packet;
+        packet << message;
         for (auto& client : this->_clients) {
-            this->_send(client, message);
+            this->_send(client, packet);
         }
     }
 
