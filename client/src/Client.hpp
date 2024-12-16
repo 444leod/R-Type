f61c82aad016ab3567e473326224185b2df76544
Thibault <thibault.longuepee@epitech.eu>
2024-12-16 17:59:38 +0100

    docs(Client): add doxygen doc to the client class

--- client/src/Client.hpp
+++ client/src/Client.hpp
@@ -11,12 +11,24 @@
 
 #pragma once
 
+/**
+ * @brief Client representation of a network Client, receives updates from a Server and sends it some
+ */
 class Client: public NetworkAgent
 {
 public:
+    /**
+     * @brief Constructor for the Client class
+     * @param ctx The io_context to add the client's work to
+     */
     Client(asio::io_context& ctx): NetworkAgent(ctx) {}
     ~Client() = default;
 
+    /**
+     * @brief Connect to a server & starts the Client's game loop
+     * @param host The server's address
+     * @param port The server's port
+     */
     void run(const std::string& host, std::uint32_t port)
     {
         const auto addr = asio::ip::address::from_string(host);
@@ -27,13 +39,24 @@ public:
         this->_window.setFramerateLimit(60);
     }
 
+    /**
+     * @brief Stops the client
+     */
     void stop()
     {
         this->_stop();
         this->_window.close();
     }
 
+    /**
+     * @brief Gets the Client's window
+     * @return A reference to the game window
+     */
     sf::RenderWindow &window() noexcept { return this->_window; }
+    /**
+     * @brief Check if the Client is still running
+     * @return `true` is the Client is running, `false` otherwise
+     */
     bool running() const noexcept { return this->_window.isOpen(); }
 
 private:
