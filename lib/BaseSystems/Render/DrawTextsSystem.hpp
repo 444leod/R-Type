edf3dbb354d64094a50eac08a5abc028c67d50de
Thibault <thibault.longuepee@epitech.eu>
2025-01-06 17:54:45 +0100

    refactor(Text): construct text from non-sfml components

--- lib/BaseSystems/Render/DrawTextsSystem.hpp
+++ lib/BaseSystems/Render/DrawTextsSystem.hpp
@@ -16,14 +16,27 @@
 class DrawTextsSystem final : public ARenderSystem
 {
 public:
-    DrawTextsSystem(Registry &registry, const std::string name = "DrawTextsSystem") : ARenderSystem(registry, name) {}
+    DrawTextsSystem(Registry &registry, const std::string name = "DrawTextsSystem") : ARenderSystem(registry, name)
+    {
+        this->_font.loadFromFile("assets/arial.ttf");
+        this->_text.setFont(this->_font);
+    }
 
-    void execute(sf::RenderWindow &window) override {
-        _registry.view<Position, Text>().each([&](const auto& entity, auto& pos, auto& renderable) {
-            renderable.text.setPosition(pos.x, pos.y);
-            window.draw(renderable.text);
+    void execute(sf::RenderWindow &window) override
+    {
+        this->_registry.view<Position, Text>().each([&](const auto &entity, auto &pos, auto &text) {
+            std::cout << "Drawing following text:\n" << text.message << std::endl;
+            this->_text.setString(text.message);
+            this->_text.setPosition(pos.x, pos.y);
+            this->_text.setCharacterSize(text.fontSize);
+            this->_text.setFillColor(sf::Color(text.color.r, text.color.g, text.color.b));
+            window.draw(this->_text);
         });
     }
+
+private:
+    sf::Text _text;
+    sf::Font _font;
 };
 
 #endif /* !DRAWTEXTSSYSTEM_HPP_ */
--- server/src/scenes/WaitingRoom/WaitingRoom.cpp
+++ server/src/scenes/WaitingRoom/WaitingRoom.cpp
@@ -17,7 +17,8 @@
 #include <SFML/Window/Keyboard.hpp>
 #include "Global.hpp"
 
-inline bool isInputAvailable() {
+inline bool isInputAvailable()
+{
     fd_set readfds;
     struct timeval tv;
     FD_ZERO(&readfds);
@@ -35,14 +36,16 @@ void WaitingRoom::initialize()
 
 void WaitingRoom::update(const double deltaTime, const sf::RenderWindow &window)
 {
-    if (isInputAvailable()) {
+    if (isInputAvailable())
+    {
         std::string input;
         if (!std::getline(std::cin, input))
         {
             if (std::cin.eof())
             {
                 _command_handlers["exit"]({});
-            } else
+            }
+            else
             {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -50,7 +53,8 @@ void WaitingRoom::update(const double deltaTime, const sf::RenderWindow &window)
         }
         else
         {
-            if (_command_handlers.contains(input)) {
+            if (_command_handlers.contains(input))
+            {
                 std::vector<std::string> args;
                 std::istringstream iss(input);
                 std::string arg;
@@ -64,7 +68,7 @@ void WaitingRoom::update(const double deltaTime, const sf::RenderWindow &window)
     }
 }
 
-void WaitingRoom::render(sf::RenderWindow& window)
+void WaitingRoom::render(sf::RenderWindow &window)
 {
     _executeRenderSystems(window);
 }
@@ -72,39 +76,45 @@ void WaitingRoom::render(sf::RenderWindow& window)
 void WaitingRoom::onEvent(sf::Event &event)
 {
     //! Will not refactor this part, not supposed to have a system here
-    if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+    if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+    {
         sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
-        _registry.view<Position, RectangleButton>().each([&](const auto& entity, auto& pos, auto& button) {
+        _registry.view<Position, RectangleButton>().each([&](const auto &entity, auto &pos, auto &button)
+                                                         {
             if (button.shape.getGlobalBounds().contains(mousePos)) {
                 button.onClick();
                 return;
-            }
-        });
+            } });
     }
 
-    switch (event.type) {
-        case sf::Event::KeyPressed:
-            switch (event.key.code) {
-                case sf::Keyboard::Space:
-                    break;
-                case sf::Keyboard::B: {
-                     break;
-                }
-                default:
-                    // _eventDispatcher.broadcast(movement_event{.key = event.key.code, .pressed = true});
-                    break;
-            }
+    switch (event.type)
+    {
+    case sf::Event::KeyPressed:
+        switch (event.key.code)
+        {
+        case sf::Keyboard::Space:
             break;
-        case sf::Event::KeyReleased:
-            switch (event.key.code) {
-                default:
-                    // _eventDispatcher.broadcast(movement_event{.key = event.key.code, .pressed = false});
-                    break;
-            }
+        case sf::Keyboard::B:
+        {
             break;
-        case sf::Event::MouseButtonPressed:
+        }
+        default:
+            // _eventDispatcher.broadcast(movement_event{.key = event.key.code, .pressed = true});
             break;
-        default:break;
+        }
+        break;
+    case sf::Event::KeyReleased:
+        switch (event.key.code)
+        {
+        default:
+            // _eventDispatcher.broadcast(movement_event{.key = event.key.code, .pressed = false});
+            break;
+        }
+        break;
+    case sf::Event::MouseButtonPressed:
+        break;
+    default:
+        break;
     }
 }
 
@@ -113,12 +123,15 @@ void WaitingRoom::onEnter()
     _registry.clear();
 
     float y = 50.0f;
-    for (const auto& client : CLIENTS) {
+    for (const auto &client : CLIENTS)
+    {
         auto entity = _registry.create();
+        Text clientText = {
+            .message = "Client " + std::to_string(client.id),
+            .fontSize = 20,
+            .color = {255, 255, 255}};
+        _registry.addComponent<Text>(entity, clientText);
         _registry.addComponent<Position>(entity, {50.0f, y});
-        sf::Text clientText("Client " + std::to_string(client.id), font, 20);
-        clientText.setFillColor(sf::Color::White);
-        _registry.addComponent<Text>(entity, {clientText});
         y += 30.0f;
     }
 
@@ -126,16 +139,18 @@ void WaitingRoom::onEnter()
     _registry.addComponent<Position>(exitButtonEntity, {20.0f, 20.0f});
     sf::RectangleShape exitButtonShape({100.0f, 40.0f});
     exitButtonShape.setFillColor(sf::Color::Red);
-    _registry.addComponent<RectangleButton>(exitButtonEntity, {exitButtonShape, "Exit", [this](){ _onExit({}); }});
+    _registry.addComponent<RectangleButton>(exitButtonEntity, {exitButtonShape, "Exit", [this]()
+                                                               { _onExit({}); }});
 
     auto startButtonEntity = _registry.create();
     _registry.addComponent<Position>(startButtonEntity, {140.0f, 20.0f});
     sf::RectangleShape startButtonShape({100.0f, 40.0f});
     startButtonShape.setFillColor(sf::Color::Green);
-    _registry.addComponent<RectangleButton>(startButtonEntity, {startButtonShape, "Start", [this](){ _onStart({}); }});
+    _registry.addComponent<RectangleButton>(startButtonEntity, {startButtonShape, "Start", [this]()
+                                                                { _onStart({}); }});
 }
 
-void WaitingRoom::onEnter(const AScene& lastScene)
+void WaitingRoom::onEnter(const AScene &lastScene)
 {
 }
 
@@ -147,11 +162,11 @@ void WaitingRoom::onExit()
     this->_broadcast(packet);
 }
 
-void WaitingRoom::onExit(const AScene& nextScene)
+void WaitingRoom::onExit(const AScene &nextScene)
 {
 }
 
-void WaitingRoom::onPacketReceived(const asio::ip::udp::endpoint& src, UDPPacket& packet)
+void WaitingRoom::onPacketReceived(const asio::ip::udp::endpoint &src, UDPPacket &packet)
 {
 
     const auto payload = packet.payload;
@@ -161,18 +176,16 @@ void WaitingRoom::onPacketReceived(const asio::ip::udp::endpoint& src, UDPPacket
     PACKET_TYPE packet_type{};
     packet >> packet_type;
 
-    auto it = std::find_if(CLIENTS.begin(), CLIENTS.end(), [&src](const auto& client) {
-        return client.endpoint == src;
-    });
+    auto it = std::find_if(CLIENTS.begin(), CLIENTS.end(), [&src](const auto &client)
+                           { return client.endpoint == src; });
 
     if (it == CLIENTS.end())
     {
         if (packet_type != PACKET_TYPE::CONNECT)
             return;
         CLIENTS.push_back(ClientInformations(src,
-            (CLIENTS.size() > 4) ? ClientInformations::Type::VIEWER : ClientInformations::Type::PLAYER,
-            CLIENTS.size()
-        ));
+                                             (CLIENTS.size() > 4) ? ClientInformations::Type::VIEWER : ClientInformations::Type::PLAYER,
+                                             CLIENTS.size()));
 
         std::cout << "New client, new size: " << CLIENTS.size() << std::endl;
 
@@ -184,15 +197,15 @@ void WaitingRoom::onPacketReceived(const asio::ip::udp::endpoint& src, UDPPacket
         _packet_handlers.at(packet_type)(*it, packet);
 }
 
-void WaitingRoom::_broadcast(const UDPPacket& packet)
+void WaitingRoom::_broadcast(const UDPPacket &packet)
 {
-    for (auto&[endpoint, type, name, id] : CLIENTS)
+    for (auto &[endpoint, type, name, id] : CLIENTS)
     {
         _manager.send(endpoint, packet);
     }
 }
 
-void WaitingRoom::_onConnect(const ClientInformations& src, UDPPacket& packet)
+void WaitingRoom::_onConnect(const ClientInformations &src, UDPPacket &packet)
 {
     std::string name;
     packet >> name;
@@ -205,29 +218,32 @@ void WaitingRoom::_onConnect(const ClientInformations& src, UDPPacket& packet)
     _manager.send(src.endpoint, response);
 
     auto entity = _registry.create();
-    _registry.addComponent<Position>(entity, {50.0f, 50.0f + 30.0f * CLIENTS.size()});
-    sf::Text clientText("Client " + std::to_string(src.id), font, 20);
-    clientText.setFillColor(sf::Color::White);
+    Text clientText = {
+        .message = "Client " + std::to_string(src.id),
+        .fontSize = 20,
+        .color = {255, 255, 255}
+    };
     _registry.addComponent<Text>(entity, {clientText});
+    _registry.addComponent<Position>(entity, {50.0f, 50.0f + 30.0f * CLIENTS.size()});
 }
 
-void WaitingRoom::_onDisconnect(const ClientInformations& src, UDPPacket& packet) {
+void WaitingRoom::_onDisconnect(const ClientInformations &src, UDPPacket &packet)
+{
     std::cout << "Client disconnected: " << src.endpoint << std::endl;
-    std::erase_if(CLIENTS, [&src](const auto& client) {
-        return client.endpoint == src.endpoint;
-    });
+    std::erase_if(CLIENTS, [&src](const auto &client)
+                  { return client.endpoint == src.endpoint; });
 
-    //send informations about players connected
+    // send informations about players connected
 }
 
-void WaitingRoom::_onMessage(const ClientInformations& source, UDPPacket& packet)
+void WaitingRoom::_onMessage(const ClientInformations &source, UDPPacket &packet)
 {
     std::string message;
     packet >> message;
     std::cout << "Message from " << source.endpoint << ": " << message << std::endl;
 }
 
-void WaitingRoom::_onExit(const std::vector<std::string>& args)
+void WaitingRoom::_onExit(const std::vector<std::string> &args)
 {
     std::cout << "Exiting..." << std::endl;
     _manager.stop();
@@ -235,7 +251,7 @@ void WaitingRoom::_onExit(const std::vector<std::string>& args)
     this->_broadcast(UDPPacket{} << PACKET_TYPE::DISCONNECT);
 }
 
-void WaitingRoom::_onStart(const std::vector<std::string>& args)
+void WaitingRoom::_onStart(const std::vector<std::string> &args)
 {
     std::cout << "Starting game..." << std::endl;
 
